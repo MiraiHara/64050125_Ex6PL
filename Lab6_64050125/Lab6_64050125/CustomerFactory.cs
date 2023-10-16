8fdf0ec43c96f0832432d9a3a9bd9811c3156519
MiraiHara <113688014+MiraiHara@users.noreply.github.com>
2023-10-17 01:19:49 +0700

    Add files via upload

---
+++ Lab6_64050125/Lab6_64050125/CustomerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CustomerFactory
+{
+    public static Customer CreateCustomer(string type)
+    {
+        if (type.Equals("Mountain", StringComparison.OrdinalIgnoreCase))
+        {
+            return new MountainCustomer();
+        }
+        else if (type.Equals("Delinquent", StringComparison.OrdinalIgnoreCase))
+        {
+            return new DelinquentCustomer();
+        }
+        else if (type.Equals("Regular", StringComparison.OrdinalIgnoreCase))
+        {
+            return new RegularCustomer();
+        }
+        return null;
+    }
+}
